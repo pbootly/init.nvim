3eb7aefba7c6ba8d5668a4c35379f4380cc3e01d
pbootly <mthomas@polybit.io>
2023-10-03 14:50:29 +0100

    Add bootstrap dependencies

---
+++ init-deps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install packer
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+# Install vim-go
+
+git clone https://github.com/fatih/vim-go.git\
+    ~/.local/share/nvim/site/pack/plugins/start/vim-go
+
+
+
