2773fc0378ae253ec1bda35e1896059fd5408c83
Matthew Thomas <mthomas@polybit.io>
2026-01-07 11:57:57 +0000

    trim the fat

--- init.lua
+++ init.lua
@@ -1,15 +1,13 @@
 vim.pack.add({
   "https://github.com/j-hui/fidget.nvim",
-  "https://github.com/mbbill/undotree",
   "https://github.com/mason-org/mason.nvim",
   "https://github.com/zootedb0t/citruszest.nvim",
   "https://github.com/stevearc/oil.nvim",
   "https://github.com/folke/snacks.nvim",
-  "https://github.com/rafamadriz/friendly-snippets",
-  "https://github.com/moyiz/blink-emoji.nvim",
   "https://github.com/mikavilpas/blink-ripgrep.nvim",
   "https://github.com/L3MON4D3/LuaSnip",
   "https://github.com/stevearc/conform.nvim",
+  "https://github.com/moyiz/blink-emoji.nvim",
   { src = "https://github.com/Saghen/blink.cmp", version = vim.version.range("^1") },
   "https://github.com/akinsho/toggleterm.nvim",
 })
