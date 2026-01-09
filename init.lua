16082730701224d2a4b84a7f309fe8b81ef5eea8
Matthew Thomas <mthomas@polybit.io>
2026-01-09 23:30:22 +0000

    kanagawa

--- init.lua
+++ init.lua
@@ -2,13 +2,14 @@ vim.pack.add({
   "https://github.com/j-hui/fidget.nvim",
   "https://github.com/mason-org/mason.nvim",
   "https://github.com/stevearc/oil.nvim",
-  "https://github.com/neanias/everforest-nvim",
+  "https://github.com/rebelot/kanagawa.nvim",
   "https://github.com/folke/snacks.nvim",
   "https://github.com/mikavilpas/blink-ripgrep.nvim",
   "https://github.com/L3MON4D3/LuaSnip",
   "https://github.com/stevearc/conform.nvim",
   { src = "https://github.com/Saghen/blink.cmp", version = vim.version.range("^1") },
   "https://github.com/akinsho/toggleterm.nvim",
+  "https://github.com/nvim-lualine/lualine.nvim",
 })
 
 vim.g.mapleader = " "
